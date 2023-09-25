416a31c0364ee55c511eae6c5e93d15e52920685
Shaun3180 <shaungeisert@gmail.com>
2023-09-24 21:22:01 -0600

    Update content.js
    Before options page

--- content.js
+++ content.js
@@ -1,22 +1,34 @@
-const theSelector = 'div[aria-label="Like"]';
-
-// Function to toggle the 'Like' button
+// Function to find and click the 'Like' button
 function clickLikeButton() {
-    const likeButtons = document.querySelectorAll(theSelector);
-    for (const button of likeButtons) {
-        // Click the 'Like' button found in the viewport
-        button.click();
+  const likeButtons = document.querySelectorAll('div[aria-label="Like"]');
+  for (const button of likeButtons) {
+    if (isInViewport(button)) {
+      // Click the 'Like' button found in the viewport
+      button.click();
 
-        // Display the date and time when the button was clicked in the console
-        //const clickedTime = new Date().toLocaleString();
-        //console.log('Clicked at:', clickedTime);
+      // Display the date and time when the button was clicked
+      const clickedTime = new Date().toLocaleString();
+      console.log('Clicked at:', clickedTime);
     }
+  }
 }
 
-// Listen for messages with the action 'clickLikeButton'
+// Function to check if an element is in the viewport
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Listen for messages from the background script
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === 'clickLikeButton') {
     // Call the clickLikeButton function when a message is received
     clickLikeButton();
   }
 });
+
