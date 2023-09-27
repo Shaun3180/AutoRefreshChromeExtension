860ba3c4307b2783349e7f333e26b171536d6e98
Shaun3180 <shaungeisert@gmail.com>
2023-09-27 07:58:04 -0600

    Update content.js
    Added in support for options page

--- content.js
+++ content.js
@@ -1,23 +1,28 @@
-// Hard-coded selector
-const theSelector = 'div[aria-label="Like"]'; // Default selector
-const likeButtons = document.querySelectorAll(theSelector);
-const minDelayBetweenElementClicks = 10 * 1000; // if there are multiple matching elements on the page, the min delay between clicks
-const maxDelayBetweenElementClicks = 100 * 1000; // if there are multiple matching elements on the page, the min delay between clicks
+let theSelector; // Stores the CSS selector for the elements to be clicked
+let minDelayBetweenElementClicks; // Minimum delay between element clicks
+let maxDelayBetweenElementClicks; // Maximum delay between element clicks
 
-// Function to find and click the 'Like' button
+// Load configuration settings from storage
+chrome.storage.sync.get(["theSelector", "minDelayBetweenElementClicks", "maxDelayBetweenElementClicks"], (result) => {
+    theSelector = result.theSelector; // Default selector if not found in storage
+    minDelayBetweenElementClicks = result.minDelayBetweenElementClicks; // Default minimum delay
+    maxDelayBetweenElementClicks = result.maxDelayBetweenElementClicks; // Default maximum delay
+});
+
+// Function to click like buttons on the page
 function clickLikeButton() {
+    const likeButtons = document.querySelectorAll(theSelector); // Find all elements matching the selector
+
     function getRandomDelay(minDelay, maxDelay) {
-        return Math.random() * (maxDelay - minDelay) + minDelay;
+        return Math.random() * (maxDelay - minDelay) + minDelay; // Generate a random delay within the specified range
     }
 
     likeButtons.forEach((button, index) => {
-        if (isInViewport(button)) {
+        if (isInViewport(button)) { // Check if the element is in the viewport
             setTimeout(() => {
-                button.click();
-
-                // Display the iteration and theSelector when the button was clicked
+                button.click(); // Click the element
                 console.log(`'${theSelector}' clicked at ${new Date().toLocaleString()}`);
-            }, getRandomDelay(minDelayBetweenElementClicks, maxDelayBetweenElementClicks) * index); // Random delay between 3-5 seconds
+            }, getRandomDelay(minDelayBetweenElementClicks, maxDelayBetweenElementClicks) * index); // Apply a random delay
         }
     });
 }
@@ -28,10 +33,9 @@ function isInViewport(element) {
     return rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
 }
 
-// Listen for messages from the background script
+// Listen for messages from the extension popup or background script
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === "clickLikeButton") {
-        // Call the clickLikeButton function when a message is received
-        clickLikeButton();
+        clickLikeButton(); // Trigger the clickLikeButton function when a message is received
     }
 });
