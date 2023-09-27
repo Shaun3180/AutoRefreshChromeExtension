998814b027019f09a388588bba9450738d31f178
Shaun3180 <shaungeisert@gmail.com>
2023-09-27 07:57:44 -0600

    Update background.js
    Adding in support for options page

--- background.js
+++ background.js
@@ -1,80 +1,114 @@
+// Global variables
 let isExtensionActive = false;
-let refreshIntervalID; // Store the interval ID for page refresh
-let liketheElementClickIntervalID; // Store the interval ID for like theElement clicking
+let refreshIntervalID;
+let liketheElementClickIntervalID;
+let minPageRefreshInterval;
+let maxPageRefreshInterval;
+let minClickDelay;
+let maxClickDelay;
+let minDelayBetweenElementClicks;
+let maxDelayBetweenElementClicks;
+let theSelector;
+let openHour;
+let closeHour;
 
-// Hard-coded values
-const minPageRefreshInterval = 10 * 60 * 1000;    // 10 minutes min before page refreshes
-const maxPageRefreshInterval = 100 * 60 * 1000;   // 100 minutes max before page refreshes
-const minClickDelay = 3 * 1000;                   // 3 seconds min after a page is refreshed before a click occurs
-const maxClickDelay = 100 * 1000;                 // 100 seconds max after a page is refrehsed before a click occurs
-const minDelayBetweenElementClicks = 10 * 1000;   // if there are multiple matching elements on the page, the min delay between clicks
-const maxDelayBetweenElementClicks = 100 * 1000;  // if there are multiple matching elements on the page, the min delay between clicks
-const theSelector = 'div[aria-label="Like"]';     // Default selector
-const openHour = 6;                             // 6:00 AM begin available time for clicks
-const closeHour = 24;                           // midnight end available time for clicks
+let configLoaded = false;
 
-// Function to click the like theElement
+// Function to load configuration settings from storage
+function loadConfig(callback) {
+    if (configLoaded) {
+        return callback();
+    }
+
+    chrome.storage.sync.get(
+        ["minPageRefreshInterval", "maxPageRefreshInterval", "minClickDelay", "maxClickDelay", "minDelayBetweenElementClicks", "maxDelayBetweenElementClicks", "theSelector", "openHour", "closeHour"],
+        function (result) {
+            // Set default values if settings are not found in storage
+            minPageRefreshInterval = parseInt(result.minPageRefreshInterval) || 10 * 1000;
+            maxPageRefreshInterval = parseInt(result.maxPageRefreshInterval) || 20 * 1000;;
+            minClickDelay = parseInt(result.minClickDelay) || 1 * 1000;
+            maxClickDelay = parseInt(result.maxClickDelay) || 2 * 1000;
+            minDelayBetweenElementClicks = parseInt(result.minDelayBetweenElementClicks) || 1 * 1000;;
+            maxDelayBetweenElementClicks = parseInt(result.maxDelayBetweenElementClicks) || 2 * 1000;;
+            theSelector = result.theSelector || 'div[aria-label="Like"]';
+            openHour = parseInt(result.openHour) || 6;
+            closeHour = parseInt(result.closeHour) || 23;
+
+            // Log variables for debugging
+            console.log("minPageRefreshInterval", minPageRefreshInterval);
+            console.log("maxPageRefreshInterval", maxPageRefreshInterval);
+            console.log("minClickDelay", minClickDelay);
+            console.log("maxClickDelay", maxClickDelay);
+            console.log("minDelayBetweenElementClicks", minDelayBetweenElementClicks);
+            console.log("maxDelayBetweenElementClicks", maxDelayBetweenElementClicks);
+            console.log("theSelector", theSelector);
+            console.log("openHour", openHour);
+            console.log("closeHour", closeHour);
+
+            configLoaded = true;
+            callback();
+        }
+    );
+}
+
+// Function to click on elements matching theSelector
 function clickLiketheElement() {
     const now = new Date();
     const currentHour = now.getHours();
 
-    // Check if the current hour is within the available hours
     if (currentHour >= openHour && currentHour < closeHour) {
-        // Execute the script in the active tab
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-            chrome.scripting.executeScript({
-                target: { tabId: tabs[0].id },
-                function: () => {
-                    // Use theSelector to find and click the given element
-                    const theElements = document.querySelectorAll(theSelector);
+            if (tabs && tabs.length > 0 && tabs[0].id) {
+                chrome.scripting.executeScript({
+                    target: { tabId: tabs[0].id },
+                    function: () => {
+                        const theElements = document.querySelectorAll(theSelector);
 
-                    function getRandomDelay(minDelay, maxDelay) {
-                        return Math.random() * (maxDelay - minDelay) + minDelay;
-                    }
-
-                    theElements.forEach((theElement, index) => {
-                        if (isInViewport(theElement)) {
-                            setTimeout(() => {
-                                theElement.click();
-
-                                // Display time when the element was clicked
-                                console.log(`'${theSelector}' clicked at ${new Date().toLocaleString()}`);
-                            }, getRandomDelay(minDelayBetweenElementClicks, maxDelayBetweenElementClicks) * index); // Random delay
+                        function getRandomDelay(minDelay, maxDelay) {
+                            return Math.random() * (maxDelay - minDelay) + minDelay;
                         }
-                    });
-                },
-            });
+
+                        theElements.forEach((theElement, index) => {
+                            if (isInViewport(theElement)) {
+                                setTimeout(() => {
+                                    theElement.click();
+                                    console.log(`'${theSelector}' clicked at ${new Date().toLocaleString()}`);
+                                }, getRandomDelay(minDelayBetweenElementClicks, maxDelayBetweenElementClicks) * index);
+                            }
+                        });
+                    },
+                });
+            }
         });
     }
 }
 
-// Function to generate a random time between minDelay and maxDelay (in milliseconds)
+// Function to generate a random delay
 function getRandomDelay(minDelay, maxDelay) {
     return Math.random() * (maxDelay - minDelay) + minDelay;
 }
 
-// Function to refresh the page and set a new random interval
+// Function to refresh the active page at a random interval
 function refreshPageWithRandomInterval() {
     refreshPage();
-
-    // Generate a new random interval for the next page refresh
     const randomInterval = getRandomDelay(minPageRefreshInterval, maxPageRefreshInterval);
-
-    // Clear the previous interval and set a new one
     clearInterval(refreshIntervalID);
     refreshIntervalID = setInterval(refreshPageWithRandomInterval, randomInterval);
 }
 
-// Function to refresh the page
+// Function to refresh the active page
 function refreshPage() {
     if (isExtensionActive) {
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-            chrome.scripting.executeScript({
-                target: { tabId: tabs[0].id },
-                function: () => {
-                    location.reload();
-                },
-            });
+            if (tabs && tabs.length > 0 && tabs[0].id) {
+                chrome.scripting.executeScript({
+                    target: { tabId: tabs[0].id },
+                    function: () => {
+                        location.reload();
+                    },
+                });
+            }
+            console.log(`Page is refreshed at ${new Date().toLocaleString()}`);
         });
     }
 }
@@ -85,46 +119,41 @@ function isInViewport(element) {
     return rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
 }
 
-// Function to toggle the extension's state
+// Function to toggle the extension's state/icon
 function toggleExtensionState() {
     isExtensionActive = !isExtensionActive;
-
-    // Clear any existing intervals
+    console.log(`Extension state toggled. Extension is now ${isExtensionActive ? "active" : "inactive"} as of ${new Date().toLocaleString()}`);
     clearInterval(refreshIntervalID);
     clearInterval(liketheElementClickIntervalID);
-
-    // Change the icon based on the extension's state
     const newIconPath = isExtensionActive ? "images/icon-active.png" : "images/icon-128.png";
-
     chrome.action.setIcon({ path: newIconPath });
 
-    // Send a message to the content script to toggle the extension state
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        // Change the action to 'clickLiketheElement'
         chrome.tabs.sendMessage(tabs[0].id, { action: "clickLiketheElement", isExtensionActive });
     });
 
-    // If extension is enabled, set up a new interval for like theElement clicking
     if (isExtensionActive) {
         liketheElementClickIntervalID = setInterval(clickLiketheElement, getRandomDelay(minClickDelay, maxClickDelay));
     }
 
-    // If extension is enabled, refresh the page with a random interval
     if (isExtensionActive) {
         refreshPageWithRandomInterval();
     }
 }
 
-// Listen for clicks on the extension icon using chrome.action
-chrome.action.onClicked.addListener(toggleExtensionState);
+// Add a listener for the extension button click event
+chrome.action.onClicked.addListener(() => {
+    loadConfig(toggleExtensionState);
+});
 
-// Initialize the extension state and setup the initial intervals
+// Check if the extension was previously active and load configuration settings
 chrome.storage.local.get("isExtensionActive", (data) => {
     if (data.isExtensionActive) {
-        toggleExtensionState(); // Enable the extension if it was previously active
+        loadConfig(toggleExtensionState);
     } else {
-        // Setup initial intervals if extension is disabled
-        refreshPageWithRandomInterval();
-        liketheElementClickIntervalID = setInterval(clickLiketheElement, getRandomDelay(minClickDelay, maxClickDelay));
+        loadConfig(() => {
+            refreshPageWithRandomInterval();
+            liketheElementClickIntervalID = setInterval(clickLiketheElement, getRandomDelay(minClickDelay, maxClickDelay));
+        });
     }
 });
